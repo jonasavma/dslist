88cfeb34f7dd091c3dfbb61abda929486509ca73
Jonas Avelino <33552721+jonasavma@users.noreply.github.com>
2023-11-14 20:15:18 -0300

    criação do projeto

---
+++ sdlist/src/test/java/br/com/sdlist/SdlistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sdlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
